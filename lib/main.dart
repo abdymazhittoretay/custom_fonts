d64ae99cb7addf1cf17aa496817e6a3ad530e2db
Toretay <abdymvzhit@gmail.com>
2025-02-24 13:38:55 +0500

    changed the fontFamily of the specific text widget

--- lib/main.dart
+++ lib/main.dart
@@ -44,13 +44,22 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          style: TextStyle(fontStyle: FontStyle.italic),
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text(
+              'You have pushed the button this many times:',
+              style: TextStyle(
+                fontFamily: "RobotoMono",
+                fontWeight: FontWeight.w700,
+              ),
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
